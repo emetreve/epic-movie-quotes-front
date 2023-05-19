97e9c1155699e55fd91b43e35a73fed8f73dab03
emetreve <emetreve@gmail.com>
2023-05-19 17:30:27 +0400

    wip: create account modal

--- components/forms/CreateAccount/CreateAccount.tsx
+++ components/forms/CreateAccount/CreateAccount.tsx
@@ -1,7 +1,7 @@
 const CreateAccount = () => {
   return (
     <div className='scrollbar-hide h-screen w-screen fixed backdrop-blur-sm bg-partly-transparent-dark text-white flex items-center justify-center top-0 left-0 z-50'>
-      <div className='bg-violet h-full w-full text-7xl'>
+      <div className='bg-violet h-full w-full lg:h-80 lg:w-36 text-7xl'>
         <h1>Form</h1>
       </div>
     </div>
--- components/landing/Poster/Poster.tsx
+++ components/landing/Poster/Poster.tsx
@@ -1,34 +1,26 @@
 import Image from 'next/image';
 import { PropsType } from './types';
 
-const Poster: React.FC<PropsType> = ({
-  image,
-  alt,
-  priority,
-  mobileTop,
-  desktopTop,
-  quote,
-  signature,
-}) => {
+const Poster: React.FC<PropsType> = (props) => {
   return (
     <article className='top-0 lg:sticky bg-cover relative lg:h-full lg:min-h-screen max-w-screen justify-center items-center'>
       <div className='h-[27rem] relative lg:h-screen w-full'>
         <div className='image-container'>
           <div className='absolute inset-0'>
             <Image
-              src={image}
-              alt={alt}
+              src={props.image}
+              alt={props.alt}
               width={1440}
               height={900}
               className='w-full h-full object-cover object-center'
-              priority={priority}
+              priority={props.priority}
             />
           </div>
         </div>
         <div
           className={`absolute text-white z-40 w-[21rem] lg:w-[64rem] pl-6 ${
-            mobileTop ? mobileTop : 'pt-36'
-          } lg:pl-40  ${desktopTop ? desktopTop : 'lg:pt-72'}`}
+            props.mobileTop ? props.mobileTop : 'pt-36'
+          } lg:pl-40  ${props.desktopTop ? props.desktopTop : 'lg:pt-72'}`}
         >
           <div className='flex flex-row'>
             <div>
@@ -36,9 +28,9 @@ const Poster: React.FC<PropsType> = ({
             </div>
             <div>
               <h1 className='text-[1.1rem] font-montserrat lg:text-5xl lg:leading-[1.4]'>
-                &quot;{quote}&quot;
+                &quot;{props.quote}&quot;
               </h1>
-              <p className='text-sm pt-4 lg:text-2xl'>{signature}</p>
+              <p className='text-sm pt-4 lg:text-2xl'>{props.signature}</p>
             </div>
           </div>
         </div>
