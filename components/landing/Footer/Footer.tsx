3338c67333ba889eb50cec1dd21ef0c70d8b06e6
emetreve <emetreve@gmail.com>
2023-05-20 22:16:38 +0400

    style: apply bg color to footer

--- components/landing/Footer/Footer.tsx
+++ components/landing/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
   return (
-    <div className='background uppercase text-[0.4rem] py-2 text-cream pl-7 lg:text-[0.8rem] lg:py-5 lg:pl-16'>
+    <div className='bg-background uppercase text-[0.4rem] py-2 text-cream pl-7 lg:text-[0.8rem] lg:py-5 lg:pl-16'>
       <p>© 2022 movie quotes. All rights reserved.</p>
     </div>
   );
