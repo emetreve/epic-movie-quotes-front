e9e657fded95c9f9c1a3c6c1bd81986b6b512967
emetreve <emetreve@gmail.com>
2023-05-23 13:13:23 +0400

    wip: prepare axios instance

--- services/axios.ts
+++ services/axios.ts
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('AUTH_TOKEN');
+  config.headers.Authorization = `Bearer ${token}`;
+
+  return config;
 });
 
 export default axiosInstance;
