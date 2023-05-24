a2d3e57e212d67de8becd74308d0e6456cbd2181
emetreve <emetreve@gmail.com>
2023-05-24 22:21:45 +0400

    fix: remove unneeded  context in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,15 @@
 import '@/styles/globals.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import type { AppProps } from 'next/app';
-import { ContextProvider, UiContextProvider } from '@/store';
+import { UiContextProvider } from '@/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <ContextProvider>
-        <UiContextProvider>
-          <Component {...pageProps} />
-        </UiContextProvider>
-      </ContextProvider>
+      <UiContextProvider>
+        <Component {...pageProps} />
+      </UiContextProvider>
     </QueryClientProvider>
   );
 }
