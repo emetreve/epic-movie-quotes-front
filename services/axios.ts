8e0d120cd68043a24c15e6524f6bb27f0bf01e36
emetreve <emetreve@gmail.com>
2023-05-24 14:14:28 +0400

    refactor: remove extra code

--- services/axios.ts
+++ services/axios.ts
@@ -4,18 +4,4 @@ const axiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`,
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  return config;
-});
-
-// axiosInstance.interceptors.response.use(
-//   async (res) => {
-//     console.log(res);
-//     return res;
-//   },
-//   async (error) => {
-//     console.log('error', error);
-//   }
-// );
-
 export default axiosInstance;
