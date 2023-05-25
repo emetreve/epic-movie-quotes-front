dda48a03b7672bc21ec4bfd1c650fb659dd47cea
emetreve <emetreve@gmail.com>
2023-05-25 16:00:17 +0400

    style: improve markup for VerifyEmail and VerifiedEmail modals

--- components/landing/VerifiedEmail/VerifiedEmail.tsx
+++ components/landing/VerifiedEmail/VerifiedEmail.tsx
@@ -6,8 +6,8 @@ const VerifiedEmail: React.FC = () => {
 
   return (
     <div className='scrollbar-hide h-screen w-screen fixed backdrop-blur-sm bg-partly-transparent-dark text-white flex items-center justify-center top-0 left-0 z-50'>
-      <div className='bg-gradient-violet lg:bg-gradient-plain-violet h-full w-full lg:h-[40rem] lg:w-[38rem] lg:rounded-2xl lg:px-[5rem] relative lg:scale-105'>
-        <div className='rounded-2xl gap-2 flex flex-col items-center justify-center h-[23rem] mt-20 bg-gradient-gray mx-8 lg:bg-gradient-plain-violet'>
+      <div className='bg-gradient-violet lg:bg-gradient-plain-violet h-full w-full lg:h-[30rem] lg:w-[38rem] lg:rounded-2xl lg:px-[5rem] relative lg:scale-105'>
+        <div className='rounded-2xl gap-2 flex flex-col items-center justify-center h-[23rem] mt-20 lg:mt-10 bg-gradient-gray mx-8 lg:bg-gradient-plain-violet'>
           <Image
             src='/assets/success-email.png'
             alt='email logo'
@@ -16,12 +16,12 @@ const VerifiedEmail: React.FC = () => {
             className='inline mr-2 h-14 w-auto'
           />
           <h1 className='text-2xl pt-2 lg:text-[2.1rem] lg:mb-1'>Thank you!</h1>
-          <p className='mt-3 text-center text-sm'>
+          <p className='mt-3 text-center text-sm lg:text-lg'>
             Your account has been activated.
           </p>
           <button
             onClick={handleClick}
-            className='mt-10 text-white bg-red py-2 lg:py-3 lg:text-xl px-6 lg:px-5 rounded-md'
+            className='mt-10 text-white bg-red py-2 lg:py-3 lg:text-xl px-6 lg:px-20 rounded-md'
           >
             Log in to your account
           </button>
--- components/landing/VerifyEmail/VerifyEmail.tsx
+++ components/landing/VerifyEmail/VerifyEmail.tsx
@@ -4,8 +4,8 @@ import { PropsType } from './types';
 const VerifyEmail: React.FC<PropsType> = ({ show, showCreateAccount }) => {
   return (
     <div className='scrollbar-hide h-screen w-screen fixed backdrop-blur-sm bg-partly-transparent-dark text-white flex items-center justify-center top-0 left-0 z-50'>
-      <div className='bg-gradient-violet lg:bg-gradient-plain-violet h-full w-full lg:h-[40rem] lg:w-[38rem] lg:rounded-2xl lg:px-[5rem] relative lg:scale-105'>
-        <div className='rounded-2xl gap-2 flex flex-col items-center justify-center h-[23rem] mt-20 bg-gradient-gray mx-8 lg:bg-gradient-plain-violet'>
+      <div className='bg-gradient-violet lg:bg-gradient-plain-violet h-full w-full lg:h-[30rem] lg:w-[38rem] lg:rounded-2xl lg:px-[5rem] relative lg:scale-105'>
+        <div className='rounded-2xl gap-2 flex flex-col items-center justify-center h-[23rem] mt-20 lg:mt-10 bg-gradient-gray mx-8 lg:bg-gradient-plain-violet'>
           <Image
             src='/assets/check-email.png'
             alt='email logo'
@@ -14,7 +14,7 @@ const VerifyEmail: React.FC<PropsType> = ({ show, showCreateAccount }) => {
             className='inline mr-2 h-14 w-auto'
           />
           <h1 className='text-2xl pt-2 lg:text-[2.1rem] lg:mb-1'>Thank you!</h1>
-          <p className='mt-3 text-center text-sm'>
+          <p className='mt-3 text-center text-sm lg:text-base lg:mt-6'>
             Please check your email and follow the instructions to activate your
             account.
           </p>
@@ -22,7 +22,7 @@ const VerifyEmail: React.FC<PropsType> = ({ show, showCreateAccount }) => {
             onClick={() => {
               show(false);
             }}
-            className='mt-10 text-white bg-red py-2 lg:py-3 lg:text-xl px-6 lg:px-5 rounded-md'
+            className='mt-10 text-white bg-red py-2 lg:py-3 lg:text-xl px-6 lg:px-24 rounded-md'
           >
             Back to homepage
           </button>
