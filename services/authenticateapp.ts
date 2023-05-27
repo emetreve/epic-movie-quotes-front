f643813d80cb33454f98513bb60f88f2256537e8
emetreve <emetreve@gmail.com>
2023-05-27 16:42:34 +0400

    refactor: content type json

--- services/authenticateapp.ts
+++ services/authenticateapp.ts
@@ -4,11 +4,7 @@ const authenticateAppInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
 });
 
-// authenticateAppInstance.defaults.headers['Content-Type'] = 'application/json';
-authenticateAppInstance.interceptors.request.use((config) => {
-  config.headers['Accept'] = 'application/json';
-  return config;
-});
+authenticateAppInstance.defaults.headers['Content-Type'] = 'application/json';
 
 authenticateAppInstance.defaults.withCredentials = true;
 
