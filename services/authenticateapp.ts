b9c0c59cef680d1e534d0e33aad955d93b83a570
emetreve <emetreve@gmail.com>
2023-05-28 13:10:20 +0400

    refactor: axios instances

--- services/authenticateapp.ts
+++ services/authenticateapp.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const authenticateAppInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
 });
-
-authenticateAppInstance.defaults.headers['Content-Type'] = 'application/json';
-
-authenticateAppInstance.defaults.withCredentials = true;
 
 export default authenticateAppInstance;
--- services/axios.ts
+++ services/axios.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
 });
-
-axiosInstance.defaults.headers['Content-Type'] = 'application/json';
-
-axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
