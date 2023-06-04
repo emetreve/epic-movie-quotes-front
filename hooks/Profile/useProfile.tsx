57f12b2fb6b85c99b57ff738539773cdfc0b3505
emetreve <emetreve@gmail.com>
2023-06-05 00:08:43 +0400

    refactor: remove comments

--- hooks/Profile/useProfile.tsx
+++ hooks/Profile/useProfile.tsx
@@ -1,15 +1,10 @@
 import { useCheckIfLoggedIn } from '@/hooks';
 import { useUiContext } from '@/store';
-// import { editUsername } from '@/services';
 
 const useProfile = () => {
   const { logged, user } = useCheckIfLoggedIn();
 
   const { showEditName, showUpdateName } = useUiContext();
-
-  // const handleChangeName = () => {
-  //   //TODO: send get request on editUsername
-  // };
 
   return { logged, user, showEditName, showUpdateName };
 };
