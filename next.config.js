3f45d9f23cedaca56c4fd7727ac8b1e3048146ab
emetreve <emetreve@gmail.com>
2023-06-07 11:56:03 +0400

    refactor: use images domain from .env

--- next.config.js
+++ next.config.js
@@ -2,14 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/storage/avatars/**',
-      },
-    ],
+    domains: [process.env.NEXT_PUBLIC_IMAGES_DOMAIN],
   },
 };
 
