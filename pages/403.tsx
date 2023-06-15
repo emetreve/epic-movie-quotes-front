2a0dcc94995166d102fca827689f11f9fc5a4337
emetreve <emetreve@gmail.com>
2023-06-16 01:00:54 +0400

    feat: translate 403 page

--- pages/403.tsx
+++ pages/403.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { GetStaticProps } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'next-i18next';
 
 const Forbidden = () => {
+  const { t } = useTranslation('statuspages');
+
   return (
     <div className='h-screen w-screen bg-gradient-violet text-white flex flex-col items-center justify-center'>
       <div className='relative w-full flex items-center justify-center mb-5'>
@@ -21,19 +26,27 @@ const Forbidden = () => {
         />
       </div>
       <h1 className='text-bold text-2xl pt-6 pb-4 lg:pt-10 lg:pb-5 lg:text-5xl'>
-        You shall not pass!
+        {t('You shall not pass!')}
       </h1>
       <div className='w-64 text-center lg:w-full lg:mt-3'>
         <p className='text-sm lg:text-2xl'>
-          Sorry, but you don&apos;t have permission to access this page
+          {t('უკაცრავად, მაგრამ თქვენ არ გაქვთ ამ გვერდზე წვდომის უფლება')}
         </p>
       </div>
       <Link href='/' className='hover:cursor-pointer'>
-        <button className='mt-7 text-white bg-red py-[0.6rem] px-6 lg:px-0 lg:w-[10rem] lg:text-xl rounded-md lg:mt-12'>
-          Return home
+        <button className='mt-7 text-white bg-red py-[0.6rem] px-6 lg:px-4 lg:w-fit lg:text-xl rounded-md lg:mt-12'>
+          {t('Return home')}
         </button>
       </Link>
     </div>
   );
 };
 export default Forbidden;
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale as string, ['statuspages'])),
+    },
+  };
+};
