87977c1fd2e1b2f29265e4ebb876692d9d65fdfa
emetreve <emetreve@gmail.com>
2023-06-21 01:57:39 +0400

    style: make movies page responsive

--- pages/dashboard/movies/index.tsx
+++ pages/dashboard/movies/index.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@/components';
+import { Header, SideProfilePanel } from '@/components';
 import { useMovies } from '@/hooks';
 import Image from 'next/image';
 import { MovieForMoviesPage } from '@/types';
@@ -9,7 +9,7 @@ const Movies = () => {
     return (
       <div
         onClick={handleOutsideClick}
-        className='min-h-screen bg-gradient-violet'
+        className='min-h-screen bg-gradient-violet relative'
       >
         <div className='h-[5rem]'>
           <Header
@@ -19,61 +19,75 @@ const Movies = () => {
             authUserId={user.id}
           />
         </div>
-        <div className='text-white px-7'>
-          <div className='flex flex-row justify-between items-center mt-2'>
-            <p className='text-xl'>My list of movies</p>
-            <button className='text-white bg-red py-[0.5rem] px-3  hover:bg-red-hover rounded-md font-thin text-[1.1rem]'>
-              <div className='flex flex-row items-center'>
-                <Image
-                  src='/assets/create-new.png'
-                  alt='create new'
-                  width={96}
-                  height={96}
-                  className='w-4 h-auto hover:cursor-pointer mr-2'
-                />
-                Add movie
-              </div>
-            </button>
-          </div>
-          <p>{`(Total ${movies ? movies.length : 0})`}</p>
+
+        <div className='w-[25%] fixed'>
+          <SideProfilePanel avatar={user.avatar} name={user.name} />
         </div>
-        <div className='px-7 pb-16'>
-          {movies &&
-            movies.map((movie: MovieForMoviesPage, index: number) => {
-              return (
-                <div
-                  key={movie.id}
-                  className={`${index === 0 ? 'mt-6' : 'mt-12'}`}
-                >
+
+        <div className='lg:px-[4rem] px-7 pb-16 lg:ml-[25%] lg:w-[75%] w-full lg:mt-[2rem]'>
+          <div className='text-white lg:mb-12'>
+            <div className='flex flex-row justify-between items-center mt-2'>
+              <div className='flex flex-row items-center'>
+                <p className='text-xl lg:text-xl'>My list of movies</p>
+                <p className='lg:block hidden ml-2 lg:text-xl'>{`(Total ${
+                  movies ? movies.length : 0
+                })`}</p>
+              </div>
+              <button className='text-white bg-red py-[0.5rem] px-3  hover:bg-red-hover rounded-md font-thin text-[1.1rem]'>
+                <div className='flex flex-row items-center'>
                   <Image
-                    src={
-                      movie.poster
-                        ? `${process.env.NEXT_PUBLIC_API_BASE_URL}${movie.poster}`
-                        : '/assets/movie-sample.png'
-                    }
+                    src='/assets/create-new.png'
                     alt='create new'
                     width={96}
                     height={96}
-                    className='w-full h-auto hover:cursor-pointer mr-2 rounded-2xl'
+                    className='w-4 h-auto hover:cursor-pointer mr-2'
                   />
-                  <p className='text-white text-lg mt-4'>
-                    {`${movie.name[locale as keyof typeof movie.name]} (${
-                      movie.year
-                    })`}
-                  </p>
-                  <div className='flex flex-row items-center text-white mt-4'>
-                    <p className='text-lg'>{movie.quotes_count}</p>
+                  Add movie
+                </div>
+              </button>
+            </div>
+            <p className='block lg:hidden'>{`(Total ${
+              movies ? movies.length : 0
+            })`}</p>
+          </div>
+          <div className='lg:grid lg:grid-cols-3 gap-x-20 lg:w-full'>
+            {movies &&
+              movies.map((movie: MovieForMoviesPage, index: number) => {
+                return (
+                  <div
+                    key={movie.id}
+                    className={`${index === 0 ? 'mt-6 lg:mt-12' : 'mt-12'}`}
+                  >
                     <Image
-                      src='/assets/quote-notification.png'
-                      alt='quote'
+                      src={
+                        movie.poster
+                          ? `${process.env.NEXT_PUBLIC_API_BASE_URL}${movie.poster}`
+                          : '/assets/movie-sample.png'
+                      }
+                      alt='create new'
                       width={96}
                       height={96}
-                      className='w-5 h-auto ml-2'
+                      className='w-full lg:w-[25rem] h-auto hover:cursor-pointer mr-2 rounded-2xl'
                     />
+                    <p className='text-white text-lg mt-4'>
+                      {`${movie.name[locale as keyof typeof movie.name]} (${
+                        movie.year
+                      })`}
+                    </p>
+                    <div className='flex flex-row items-center text-white mt-4'>
+                      <p className='text-lg'>{movie.quotes_count}</p>
+                      <Image
+                        src='/assets/quote-notification.png'
+                        alt='quote'
+                        width={96}
+                        height={96}
+                        className='w-5 h-auto ml-2'
+                      />
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+          </div>
         </div>
       </div>
     );
