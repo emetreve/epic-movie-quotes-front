bb23260cc5c0630656acbf4647c77c380cbde5c0
emetreve <emetreve@gmail.com>
2023-06-29 20:10:10 +0400

    refactor: remove comment

--- hooks/Profile/useProfile.tsx
+++ hooks/Profile/useProfile.tsx
@@ -65,23 +65,6 @@ const useProfile = () => {
     }
   };
 
-  // useEffect(() => {
-  //   if (status === 'success') {
-  //     setShowSuccess(true);
-  //   }
-  //   setTimeout(() => {
-  //     setShowSuccess(false);
-  //     router.push({
-  //       pathname: router.pathname,
-  //       query: {},
-  //     });
-  //   }, 4000);
-
-  //   if (changeEmail && changeEmail?.length > 0) {
-  //     editEmail(changeEmail as string);
-  //   }
-  // }, [status, changeEmail]);
-
   useEffect(() => {
     let timeoutId: NodeJS.Timeout;
 
